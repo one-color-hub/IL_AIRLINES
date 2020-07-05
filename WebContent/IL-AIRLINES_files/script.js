2b1817a4042104c9c9e25ba94da4b3ab45df99dc
one-color-hub <shogo1997.i@gmail.com>
2020-07-05 13:51:15 +0900

    0705登録

---
+++ WebContent/IL-AIRLINES_files/script.js
@@ -0,0 +1,7 @@
+/*$( window ).ready(function(){
+    var video = document.getElementsByTagName('video')[0];
+    video.addEventListener('ended', function(){
+	$(".wrap-main-visual.video").addClass('ended');
+	$(".wrap-main-visual.picture").removeClass('hide');
+    },false)
+});*/
